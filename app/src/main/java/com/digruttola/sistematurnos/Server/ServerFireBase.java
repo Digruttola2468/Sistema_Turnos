2d469d791bc4aa52e872f245fc47c20e4087444a
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-05-14 15:57:48 -0300

    Conexion con FireBase

---
+++ app/src/main/java/com/digruttola/sistematurnos/Server/ServerFireBase.java
@@ -0,0 +1,7 @@
+package com.digruttola.sistematurnos.Server;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ServerFireBase {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+}
