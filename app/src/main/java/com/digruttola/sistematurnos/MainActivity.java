3c68427447e7d6b3e398e98e3f21e36cdfb3089f
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-05-14 13:02:40 -0300

    Creando dialog Hora y Fecha

--- app/src/main/java/com/digruttola/sistematurnos/MainActivity.java
+++ app/src/main/java/com/digruttola/sistematurnos/MainActivity.java
@@ -2,14 +2,22 @@ package com.digruttola.sistematurnos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 /*
 * Vista del Dentista
 * Agregar , Modificar , eliminar y leer Turnos
@@ -17,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
 *
 * */
 
-    Spinner dia,mes,anio;
+    private Button btFecha , btHora;
+    private EditText findFecha,findHora;
+    private int dia,mes,anio,hora,minutos;
 
 
     @Override
@@ -25,9 +35,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dia = findViewById(R.id.sp_dia);
-        mes = findViewById(R.id.sp_mes);
-        anio = findViewById(R.id.sp_anio);
+        btFecha = findViewById(R.id.bt_Fecha);
+        btHora = findViewById(R.id.bt_Hora);
+        findFecha = findViewById(R.id.edt_fecha);
+        findHora = findViewById(R.id.edt_hora);
 
+        btFecha.setOnClickListener(this);
+        btHora.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btFecha){
+            final Calendar c = Calendar.getInstance();
+            dia = c.get(Calendar.DAY_OF_MONTH);
+            mes = c.get(Calendar.MONTH);
+            anio = c.get(Calendar.YEAR);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        findFecha.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                    }
+                },anio,mes,dia);
+                datePickerDialog.show();
+            }
+        }
+        if(v == btHora){
+            final Calendar c = Calendar.getInstance();
+            hora = c.get(Calendar.HOUR_OF_DAY);
+            minutos = c.get(Calendar.MINUTE);
+            Log.d("FECHA",hora+":"+minutos);
+
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    findHora.setText(hourOfDay+":"+minute);
+                }
+            },hora,minutos,true);
+            timePickerDialog.show();
+        }
     }
 }
